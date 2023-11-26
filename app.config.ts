c274de7d549d1c82d08f11e76f35c2152ef094d1
Fhyrox <144560014+fhyrox@users.noreply.github.com>
2023-11-26 12:43:48 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,22 +1,24 @@
 export default defineAppConfig({
-  docus: {
-    title: 'Bozkurt',
-    description: 'Bozkurt Development resmi sitesi.',
-    socials: {
-      github: 'BozkurtDevelopment',
-      discord: {
-         label: 'Discord',
-        icon: 'simple-icons:discord',
-        href: 'https://discord.gg/2evpFX22TB'
+   "docus":{
+      "title":"Bozkurt",
+      "description":"Bozkurt Development resmi sitesi.",
+      "socials":{
+         "github":"BozkurtDevelopment",
+         "discord":{
+            "label":"Discord",
+            "icon":"simple-icons:discord",
+            "href":"https://discord.gg/2evpFX22TB"
+         }
+      },
+      "aside":{
+         "level":0,
+         "exclude":[
+            
+         ]
+      },
+      "header":{
+         "title":"Bozkurt Development",
+         "logo":false
       }
-    },
-    aside: {
-      level: 0,
-      exclude: []
-    },
-    header: {
-      title: "Bozkurt Development",
-      logo: false
-    },
-  }
+   }
 })
